83281b94f5bfd8b05f76260463477150908a0e4e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-12 19:25:01 +0800

    Delete blog-gateway directory

--- blog-gateway/config/default.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    port: 5000,
-    mongo: {
-        url: 'mongodb://127.0.0.1:27017/zhacker-blog',
-        opts: {}
-    }
-}
--- blog-gateway/services/draft.js
+++
@@ -1,5 +0,0 @@
-const Draft = require('../models').Draft
-
-exports.createDraft = (draft)=>{
-    return Draft.create(draft)
-}
